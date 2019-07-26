eec9317a6afe58fdf6c2b5553b5fb9d0aa771236
Cenk Ergül <cenk.erguel@web.de>
2019-07-26 17:03:01 +0200

    Add more columns like "user_id", "job_status", etc. and better automated iteration through the articles / ratings

--- ScrapeWithNavbar.py
+++ ScrapeWithNavbar.py
@@ -4,13 +4,14 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+page_num = 0
+sum_articles = 0
+user_ID = 0
 
 url = "https://www.kununu.com/de/gothaer-versicherungsbank-vvag"
 url = url+'/kommentare'
 driver = webdriver.Safari()
 driver.get(url)
-page_num = 0
-sum_div = 0
 
 
 # Das muss nach den Clicks kommen und dann jedes mal wenn der nächste Reiter durchgeklickt wird nochmal!
@@ -19,87 +20,103 @@ soup = BeautifulSoup(page_source, 'lxml')
 
 csv_file = open('scrape_new.csv', 'w')
 csv_writer = csv.writer(csv_file, delimiter=';')
-csv_writer.writerow(['Company', 'City', 'Jobstatus', 'Position', 'Business Unit', 'Header', 'Text', 'Rating'])
-
+csv_writer.writerow(['ID', 'Company', 'City', 'Jobstatus', 'Position', 'Business Unit', 'Header', 'Text', 'Rating'])
 
 time.sleep(5)
+
+
 
 # Click through the second navbar "Mitarbeiter", "Bewerber", "Azubis", etc.
 for div in soup.find_all('nav', class_='navbar company-profile-subnav'):
     for li in div.find_all('li'):
         navbar_item = li.text
-        print(li.text)
+        print(navbar_item)
         driver.find_element_by_link_text(navbar_item).click()
+        navbar_item_number = int(li.span.text)
+        print(navbar_item_number)
 
+        control_value = 0
         hasLoadMore = True
-        while hasLoadMore:
-            time.sleep(5)
+        while control_value != navbar_item_number:
+
             try:
                 if driver.find_elements_by_css_selector('.btn.btn-default.btn-block.ng-isolate-scope'):
                     driver.find_element_by_css_selector('.btn.btn-default.btn-block.ng-isolate-scope').click()
-                    page_num += 1
-                    print("getting page number "+str(page_num))
-                    time.sleep(5)
-                else:
-                    hasLoadMore = False
-            except:
-                hasLoadMore = False
 
-        # for review_details in soup.find_all('div', class_='review-details user-content hidden-xs'):
-        #     for details in review_details.find_all('div', class_='text-sm text-gray-base-70 text-light text-uppercase'):
-        #
-        #         # Correct formatting
-        #         format_details = details.text.replace('\n', ' ').strip()
-        #         correct_details = details.next_sibling.text.replace('\n', ' ').strip()
-        #
-        #         if format_details == 'Firma':
-        #             company = correct_details
-        #         elif format_details == 'Stadt':
-        #             city = correct_details
-        #         elif format_details == 'Jobstatus':
-        #             jobstatus = correct_details
-        #         elif format_details == 'Position/Hierarchie':
-        #             position = correct_details
-        #         elif format_details == 'Unternehmensbereich':
-        #             business_unit = correct_details
+                page_source = driver.page_source
+                soup = BeautifulSoup(page_source, 'lxml')
+
+                control_value = len(soup.find_all('article', class_='company-profile-review'))
+                print(control_value)
+
+            except:
+
+                page_source = driver.page_source
+                soup = BeautifulSoup(page_source, 'lxml')
+
+                control_value = len(soup.find_all('article', class_='company-profile-review'))
+                print('EXCEPT', control_value)
+
 
         page_source = driver.page_source
         soup = BeautifulSoup(page_source, 'lxml')
 
-        # Find all Review-Bodies, which include the reviews
-        for review_body in soup.find_all('div', class_='review-body'):
+        sum_articles = len(soup.find_all('article', class_='company-profile-review'))
+        print(sum_articles)
 
-            #print(len(soup.find_all('div', class_='review-body')))
-            #sum_div = len(soup.find_all('div', class_='review-content')) + sum_div
+        for art in soup.find_all('article', class_='company-profile-review'):
 
-            # find every h2-Tag in review-body, e.g. "Arbeitsatmosphäre"
-            for header in review_body.find_all('h2'):
+            # Find all Review-Bodies, which include the reviews
+            for review_body in art.find_all('div', class_='review-body'):
 
-                # just extract the text, not the tags
-                h2 = header.text
-                p1 = header.next_sibling
-                p2 = p1.next_sibling
+                # find every h2-Tag in review-body, e.g. "Arbeitsatmosphäre"
+                for header in review_body.find_all('h2'):
 
-                if p1.text == '' and p2 is not None:
-                    p = p2.text.replace('\n', ' ').strip()
+                    # just extract the text, not the tags
+                    h2 = header.text
+                    p1 = header.next_sibling
+                    p2 = p1.next_sibling
 
-                else:
-                    p = p1.text.replace('\n', ' ').strip()
+                    if p1.text == '' and p2 is not None:
+                        p = p2.text.replace('\n', ' ').strip()
 
-                # to iterate over the correct rating-group class, the .next_sibling is used, because otherwise it takes just the ratings of the first rating-group
-                for rating_group in review_body.next_sibling.find_all('div', class_='rating-group'):
-                    rating_h2 = rating_group.span.text.replace('\n', ' ').strip()
-                    rating_star = rating_group.div.span.text.replace('\n', ' ').strip()
-                    h2
-                    p
+                    else:
+                        p = p1.text.replace('\n', ' ').strip()
 
-                    # check if the rating_h2 is the same as the h2 of the review-body
-                    if rating_h2 == h2:
-                        #csv_writer.writerow([company, city, jobstatus, position, business_unit, h2, p, rating_star])
-                        csv_writer.writerow([h2, p, rating_star])
+                    # to iterate over the correct rating-group class, the .next_sibling is used, because otherwise it takes just the ratings of the first rating-group
+                    for rating_group in review_body.next_sibling.find_all('div', class_='rating-group'):
+                        rating_h2 = rating_group.span.text.replace('\n', ' ').strip()
+                        rating_star = rating_group.div.span.text.replace('\n', ' ').strip()
+                        h2
+                        p
+
+                        # check if the rating_h2 is the same as the h2 of the review-body
+                        if rating_h2 == h2:
+
+                            user_ID = user_ID + 1
+
+                            # Include the sidebar information as well
+                            for review_details in art.find_all('div', class_='review-details user-content hidden-xs'):
+                                for details in review_details.find_all('div', class_='text-sm text-gray-base-70 text-light text-uppercase'):
+
+                                    # Correct formatting
+                                    format_details = details.text.replace('\n', ' ').strip()
+                                    correct_details = details.next_sibling.text.replace('\n', ' ').strip()
+
+                                    if format_details == 'Firma':
+                                        company = correct_details
+                                    elif format_details == 'Stadt':
+                                        city = correct_details
+                                    elif format_details == 'Jobstatus':
+                                        jobstatus = correct_details
+                                    elif format_details == 'Position/Hierarchie':
+                                        position = correct_details
+                                    elif format_details == 'Unternehmensbereich':
+                                        business_unit = correct_details
+                            csv_writer.writerow([user_ID, company, city, jobstatus, position, business_unit, h2, p, rating_star])
 
 csv_file.close()
-#print(sum_div)
+print(sum_articles)
 
 #driver.quit()
 
